96a7db702a7d8dd9ac8914354bda5ba6cf5931a7
Planeshifter <pburckhardt@outlook.com>
2015-07-20 22:14:10 +0200

    [INIT]

---
+++ test/fixtures/test.array.R
@@ -0,0 +1,17 @@
+options( digits = 16 );
+library( jsonlite );
+
+
+v = 1
+x = seq( -1000, 1000, 0.5 )
+y = dt( x, v )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	v = v,
+	data = x,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/array.json" )
